a282dbbe6237749cf5d46615c6f39f259a188106
Nadnerde <115773895+Clt689@users.noreply.github.com>
2024-03-19 18:51:19 +0900

    [level 0] Title: 가까운 수, Time: 3.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.js
@@ -0,0 +1,15 @@
+function solution(array, n) {
+    let answer = 0;
+    let minAbs = 999999;
+    
+    const sortedArray = array.sort((a,b) => a - b);
+    console.log(sortedArray);
+    for(let i=0; i < sortedArray.length; i++){
+        const item = sortedArray[i];
+        if(minAbs > Math.abs(n - item)){
+            minAbs = Math.abs(n - item);
+            answer = item;
+        }
+    }
+    return answer;
+}
