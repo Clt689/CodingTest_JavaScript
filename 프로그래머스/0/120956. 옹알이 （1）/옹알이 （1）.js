84fc0404090b66b02bf04abbba0465a3774ab2ea
이한빈 <115773895+Clt689@users.noreply.github.com>
2024-05-13 12:00:48 +0900

    [level 0] Title: 옹알이 (1), Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -0,0 +1,20 @@
+function solution(babbling) {
+    var answer = 0;
+    const a = ["aya", "ye", "woo", "ma"];
+    
+    //발음해야 할 문자열 순회
+    babbling.forEach(ba => {
+        // 발음 가능한 문자열 순회
+        a.forEach(a => {
+            // 발음할 수 있는 문자열이라면?
+            if(ba.includes(a)) {
+                // split을 통해 문자열을 쪼개고 공백을 포함시켜 합침
+                ba = ba.split(a).join(' ');
+            }
+        })
+        
+        //공백을 제외한 문자열의 길이가 0이라면 카운트
+            if(ba.trim().length === 0) answer++;
+    })
+    return answer;
+}
