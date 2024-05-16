0a0ca4fbfc33cfc54253ea202e7f39a7aac025c8
이한빈 <115773895+Clt689@users.noreply.github.com>
2024-05-16 23:07:48 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -0,0 +1,15 @@
+function solution(price, money, count) {
+    let answer = -1;
+    let sum = 0;
+    for(let i = 1; i <= count; i++){
+        sum += price * i;
+    }
+    if ( sum <= money ){
+        answer = 0;
+    }
+    else {
+        answer = sum - money;
+    }
+
+    return answer;
+}
