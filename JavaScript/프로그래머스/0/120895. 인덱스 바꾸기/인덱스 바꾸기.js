cd8da3d0ff133e98d4b1511f80337d49fccebe15
이한빈 <115773895+Clt689@users.noreply.github.com>
2025-11-15 17:19:05 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ JavaScript/프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,15 +1,11 @@
 function solution(my_string, num1, num2) {
     var answer = '';
 
-    for(let i=0; i < my_string.length; i++){
-        if ( i == num1 ){
-            answer += my_string[num2];
-            continue;
-        } else if ( i == num2 ){
-            answer += my_string[num1];
-            continue;
-        }
-        answer += my_string[i];
-    }
+    answer = my_string.split('');
+    
+    // 구조 분해 할당은 배열 or 객체에서 사용
+    [ answer[num1], answer[num2] ] = [ answer[num2], answer[num1] ];
+    
+    answer = answer.join('');
     return answer;
 }
